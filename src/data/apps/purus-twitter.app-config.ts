606574ba058e58194e267c44e9eef32b4de888a3
hocker <93564256+hockerdiscord@users.noreply.github.com>
2021-12-20 20:46:09 +0700

    Update purus-twitter.app-config.ts

--- src/data/apps/purus-twitter.app-config.ts
+++ src/data/apps/purus-twitter.app-config.ts
@@ -1,11 +1,1 @@
-import { createAppConfig } from '__/helpers/create-app-config';
 
-export const purusTwitterAppConfig = createAppConfig({
-  title: `Puru's Twitter`,
-  resizable: true,
-
-  shouldOpenWindow: false,
-  externalAction: () => window.open('https://twitter.com/puruvjdev', '_blank'),
-
-  dockBreaksBefore: true,
-});
